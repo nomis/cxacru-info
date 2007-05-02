cd36bbd4c1eae3168647a92dabc80b928fe8ef8c
Simon Arlott <simon.arlott.org.uk>
2007-05-02 23:15:20 +0000

    add program url

--- cxacru-info.c
+++ cxacru-info.c
@@ -19,6 +19,8 @@
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 	Or, point your browser to http://www.gnu.org/copyleft/gpl.html
 
+
+	http://simon.arlott.org/sw/cxacru-info/
 	$Id$
 */
 
